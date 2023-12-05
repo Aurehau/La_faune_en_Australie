001c3f6a65df78a2d25a0553844e8f066285fc5d
Aurehau <114416513+Aurehau@users.noreply.github.com>
2023-12-05 11:31:52 +0100

    a

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 const ctx = document.getElementById('myChart');
 
+const ctx2 = document.getElementById('myChart2');
+
   new Chart(ctx, {
     type: 'bar',
     data: {
@@ -18,3 +20,29 @@ const ctx = document.getElementById('myChart');
       }
     }
   });
+
+
+  const data = {
+    labels: [
+      'Red',
+      'Blue',
+      'Yellow'
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [300, 50, 100],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)'
+      ],
+      hoverOffset: 4
+    }]
+  };
+
+  const config = {
+    type: 'pie',
+    data: data,
+  };
+
+  new Chart(ctx2,config);
