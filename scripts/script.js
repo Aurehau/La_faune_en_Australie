ccf1ae00ea8fb46c15ae078e28837dd60eb6943a
Aurehau <114416513+Aurehau@users.noreply.github.com>
2023-12-30 13:24:40 +0100

    essai chart js

--- scripts/script.js
+++ scripts/script.js
@@ -19,10 +19,36 @@ const ctx2 = document.getElementById('myChart2');
     }]
   };
 
+  const plugin = {
+    id: 'customCanvasBackgroundColor',
+    beforeDraw: (chart, args, options) => {
+      const {ctx} = chart;
+      ctx.save();
+      ctx.globalCompositeOperation = 'destination-over';
+      ctx.fillStyle = options.color || '#99ffff';
+      ctx.fillRect(0, 0, chart.width, chart.height);
+      ctx.restore();
+    }
+  };
+
   const config = {
     type: 'pie',
     data: data,
     rotation : '200',
+    options: {
+      plugins: {
+        customCanvasBackgroundColor: {
+          color: 'rgba(255, 0, 0, 0)',
+        },
+        legend: {
+          display: false,
+          labels: {
+              color: 'rgb(255, 99, 132)'
+          }
+        }
+      }
+    },
+    plugins: [plugin]
   };
 
   
