b6957caac8bf37f9177010d6dccb4051f7703422
Aurehau <114416513+Aurehau@users.noreply.github.com>
2023-12-31 15:45:16 +0100

    texte sur curseur

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 const régime=[
-  {type:"carnivores", part:10},
-  {type:"omnivores", part: 7},
-  {type:"herbivore", part:90}
+  {type:"Carnivores", part:10},
+  {type:"Omnivores", part: 7},
+  {type:"Herbivore", part:90}
   ];
 
 //10 ; 4 ; espèces
@@ -14,60 +14,64 @@ function camembert(table) {
   let code="";
   let coordonnéesABC=[0,0,100,100,0,0] ;
   let n = 0;
+  let info ="";
 
   table.forEach((element) => 
   total+=element.part);
 
   table.forEach(function(element) { 
-  pourcentage=(element.part*100)/total; 
-  degré=(element.part*360)/total;
+    pourcentage=(element.part*100)/total; 
+    degré=(element.part*360)/total;
 
-  n+=1;
+    n+=1;
 
-  totalDegré+=degré;
+    totalDegré+=degré;
 
-  coordonnéesABC[0]=coordonnéesABC[4];
-  coordonnéesABC[1]=coordonnéesABC[5];
+    coordonnéesABC[0]=coordonnéesABC[4];
+    coordonnéesABC[1]=coordonnéesABC[5];
 
-  if((totalDegré>=0) && (totalDegré<=90)){
-    coordonnéesABC[4]=(totalDegré*100)/90;
-    coordonnéesABC[5]=0;
+    if((totalDegré>=0) && (totalDegré<=90)){
+      coordonnéesABC[4]=(totalDegré*100)/90;
+      coordonnéesABC[5]=0;
 
-  } else if(totalDegré>90 && totalDegré<=180){
-    coordonnéesABC[4]=100;
-    coordonnéesABC[5]=((totalDegré-90)*100)/90;
+    } else if(totalDegré>90 && totalDegré<=180){
+      coordonnéesABC[4]=100;
+      coordonnéesABC[5]=((totalDegré-90)*100)/90;
 
-  } else if(totalDegré>180 && totalDegré<=270){
-    coordonnéesABC[4]=100-(((totalDegré-180)*100)/90);
-    coordonnéesABC[5]=100;
+    } else if(totalDegré>180 && totalDegré<=270){
+      coordonnéesABC[4]=100-(((totalDegré-180)*100)/90);
+      coordonnéesABC[5]=100;
 
-  } else if(totalDegré>270 && totalDegré<=360){
-    coordonnéesABC[4]=0;
-    coordonnéesABC[5]=100-(((totalDegré-270)*100)/90);
+    } else if(totalDegré>270 && totalDegré<=360){
+      coordonnéesABC[4]=0;
+      coordonnéesABC[5]=100-(((totalDegré-270)*100)/90);
 
-  }
+    }
 
-  if((coordonnéesABC[4]>0 && coordonnéesABC[5]==0)||(coordonnéesABC[4]==100 && coordonnéesABC[5]<100)){
-    coordonnéesABC[2]=coordonnéesABC[4];
-    coordonnéesABC[3]=coordonnéesABC[5];
-  } else if((coordonnéesABC[0]>0 && coordonnéesABC[1]==100)||(coordonnéesABC[0]==0 && coordonnéesABC[1]>0)){
-    coordonnéesABC[2]=coordonnéesABC[0];
-    coordonnéesABC[3]=coordonnéesABC[1];
-  } else{
-    coordonnéesABC[2]=100;
-    coordonnéesABC[3]=100;
-  }
+    if((coordonnéesABC[4]>0 && coordonnéesABC[5]==0)||(coordonnéesABC[4]==100 && coordonnéesABC[5]<100)){
+      coordonnéesABC[2]=coordonnéesABC[4];
+      coordonnéesABC[3]=coordonnéesABC[5];
+    } else if((coordonnéesABC[0]>0 && coordonnéesABC[1]==100)||(coordonnéesABC[0]==0 && coordonnéesABC[1]>0)){
+      coordonnéesABC[2]=coordonnéesABC[0];
+      coordonnéesABC[3]=coordonnéesABC[1];
+    } else{
+      coordonnéesABC[2]=100;
+      coordonnéesABC[3]=100;
+    }
 
-  code+=`<div style="--pointAX: ${coordonnéesABC[0]}%; --pointAY: ${coordonnéesABC[1]}%; --pointBX: ${coordonnéesABC[2]}%; --pointBY: ${coordonnéesABC[3]}%; --pointCX: ${coordonnéesABC[4]}%; --pointCY: ${coordonnéesABC[5]}%; " class="part part${n}" onmousemove="curseur(this)" ></div>`; });
-  return code+`<div class='cursor' id="cursor"></div>`;
+    code+=`<div style="--pointAX: ${coordonnéesABC[0]}%; --pointAY: ${coordonnéesABC[1]}%; --pointBX: ${coordonnéesABC[2]}%; --pointBY: ${coordonnéesABC[3]}%; --pointCX: ${coordonnéesABC[4]}%; --pointCY: ${coordonnéesABC[5]}%; " class="part part${n}" onmousemove="curseur(this)" ></div>`; 
+    info+=`<div class='info info${n}'><div class='type'>${element.type}</div><div class='pourcentage'>${pourcentage}</div><div class='nbespeces'><span class='nb'>${element.part}</span> espèces</div></div>`;
+  });
+
+  return code+`<div class='cursor' id="cursor">`+info+`</div>`;
 }
 
 document.querySelector(".marsupiales").innerHTML = camembert(régime);
 
-  const poisson_insect=[
-    {type:"Endémique", part:90},
-    {type:"Venu d'ailleurs", part: 10}
-    ];
+const poisson_insect=[
+  {type:"Endémique", part:90},
+  {type:"Venu d'ailleurs", part: 10}
+  ];
 
-    document.querySelector(".poisson_insect").innerHTML = camembert(poisson_insect);
+document.querySelector(".poisson_insect").innerHTML = camembert(poisson_insect);
  
